5be105c3c4db19f67d55d10d142db40e766fc02b
tf <tf@deb-bbwm-j>
2021-04-02 23:14:57 -0500

    add / remove

--- README.md
+++
@@ -1,6 +0,0 @@
-# dotfiles
-linux OS, 自分のドットファイルを保存。
-
-to install run:
-
-sudo bash dotfilesLink.sh
--- debian/dotfilesLink.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-ln -sf ~/dotfiles/.config/nvim/init.vim ~/.config/nvim/init.vim
-ln -sf ~/dotfiles/.config/nvim/colors   ~/.config/nvim/colors
-ln -sf ~/dotfiles/.config/nvim/.cheetsheet.md   ~/.config/nvim/.cheetsheet.md
-
-
